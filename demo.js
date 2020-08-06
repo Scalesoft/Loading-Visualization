b063f8b89757c41fc53193f393cfe573f4ee9d32
nikita.iryupin <nikita.iryupin@scalesoft.cz>
2020-08-06 15:41:45 +0200

    Js Fixing error on IE

--- demo.js
+++ demo.js
@@ -15,14 +15,14 @@ const finite = [
 ];
 
 function resetInfinite() {
-    for (let element of infinite) {
-        $("#" + element).attr("hidden", true);
+    for (let i = 0; i < infinite.length; i++){
+        $("#" + infinite[i]).attr("hidden", true);
     }
 }
 
 function resetFinite() {
-    for (let element of finite) {
-        $("#" + element).attr("hidden", true);
+    for (let i = 0; i < finite.length; i++){
+        $("#" + finite[i]).attr("hidden", true);
     }
 }
 $(document).ready(function() {
@@ -31,18 +31,18 @@ $(document).ready(function() {
     loader.initLoaderAll();
     loader.startObserving();
 
-    for (let element of infinite) {
-        $("#" + element + "_button").unbind().click(function(event) {
+    for (let i = 0; i < infinite.length; i++){
+        $("#" + infinite[i] + "_button").unbind().click(function(event) {
             event.preventDefault();
             resetInfinite();
-            $("#" + element).attr("hidden", false);
+            $("#" + infinite[i]).attr("hidden", false);
         });
     }
-    for (let element of finite) {
-        $("#" + element + "_button").unbind().click(function(event) {
+    for (let i = 0; i < finite.length; i++) {
+        $("#" + finite[i] + "_button").unbind().click(function(event) {
             event.preventDefault();
             resetFinite();
-            $("#" + element).attr("hidden", false);
+            $("#" + finite[i]).attr("hidden", false);
         });
     }
     $("#finite").unbind().click(function(event) {
@@ -59,90 +59,92 @@ $(document).ready(function() {
         $("#infinite").addClass("active");
         $("#infinite_container").attr("hidden", false);
     });
+    
     let spinners = document.getElementsByClassName("lv-determinate_circle");
     let lines = document.getElementsByClassName("lv-determinate_line");
     let borderedLines = document.getElementsByClassName("lv-determinate_bordered_line");
+
     $("#fill_spinner").unbind().click(function(event) {
         event.preventDefault();
-        for(let spinner of spinners) {
-            let circle = lv.create(spinner);
+        for(let i = 0; i < spinners.length; i++) {
+            let circle = lv.create(spinners[i]);
             circle.fill(200);
         }
     });
     $("#add_spinner").unbind().click(function (event) {
        event.preventDefault();
-       for(let spinner of spinners) {
-           let circle = lv.create(spinner);
+       for(let i = 0; i < spinners.length; i++) {
+           let circle = lv.create(spinners[i]);
            circle.add(5, 200);
        }
     });
     $("#remove_spinner").unbind().click(function (event) {
         event.preventDefault();
-        for(let spinner of spinners) {
-            let circle = lv.create(spinner);
+        for(let i = 0; i < spinners.length; i++) {
+            let circle = lv.create(spinners[i]);
             circle.add(-10, 200);
         }
     });
     $("#reset_spinner").unbind().click(function (event) {
         event.preventDefault();
-        for(let spinner of spinners) {
-            let circle = lv.create(spinner);
+        for(let i = 0; i < spinners.length; i++) {
+            let circle = lv.create(spinners[i]);
             circle.reset(200);
         }
     });
     $("#fill_line").unbind().click(function(event) {
         event.preventDefault();
-        for(let line of lines) {
-            let el = lv.create(line);
+        for(let i = 0; i < lines.length; i++) {
+            let el = lv.create(lines[i]);
             el.fill(200);
         }
     });
     $("#add_line").unbind().click(function (event) {
         event.preventDefault();
-        for(let line of lines) {
-            let el = lv.create(line);
+        for(let i = 0; i < lines.length; i++) {
+            let el = lv.create(lines[i]);
             el.add(5, 200);
         }
     });
     $("#remove_line").unbind().click(function (event) {
         event.preventDefault();
-        for(let line of lines) {
-            let el = lv.create(line);
+        for(let i = 0; i < lines.length; i++) {
+            let el = lv.create(lines[i]);
             el.add(-10, 200);
         }
     });
     $("#reset_line").unbind().click(function (event) {
         event.preventDefault();
-        for(let line of lines) {
-            let el = lv.create(line);
+        for(let i = 0; i < lines.length; i++) {
+            let el = lv.create(lines[i]);
             el.reset(200);
         }
     });
     $("#fill_bordered_line").unbind().click(function(event) {
         event.preventDefault();
-        for(let line of borderedLines) {
-            let el = lv.create(line);
+        for(let i = 0; i < borderedLines.length; i++) {
+            let el = lv.create(borderedLines[i]);
             el.fill(200);
         }
     });
     $("#add_bordered_line").unbind().click(function (event) {
         event.preventDefault();
-        for(let line of borderedLines) {
-            let el = lv.create(line);
+        for(let i = 0; i < borderedLines.length; i++) {
+            let el = lv.create(borderedLines[i]);
             el.add(5, 200);
         }
     });
     $("#remove_bordered_line").unbind().click(function (event) {
         event.preventDefault();
-        for(let line of borderedLines) {
-            let el = lv.create(line);
+        for(let i = 0; i < borderedLines.length; i++) {
+            let el = lv.create(borderedLines[i]);
             el.add(-10, 200);
         }
     });
     $("#reset_bordered_line").unbind().click(function (event) {
         event.preventDefault();
-        for(let line of borderedLines) {
-            let el = lv.create(line);
+        for(let i = 0; i < borderedLines.length; i++) {
+            let el = lv.create(borderedLines[i]);
             el.reset(200);
         }
     });
